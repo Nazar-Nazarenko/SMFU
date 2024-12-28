8b01434d97dfcfb7612b8afec0c72de51069bb4e
nazarnazarenko <nanazarenko@deloitte.com>
2024-12-28 10:51:11 +0200

    initial commit

---
+++ static/main.js
@@ -0,0 +1,16 @@
+const res = document.querySelector('.reserve-container')
+const act = document.querySelector('.action-container')
+
+function openReserve() {
+    console.log("open reserve");
+    console.log("close action");
+    act.style.display = 'none';
+    res.style.display = 'block';
+}
+
+function openAction() {
+    console.log("open action");
+    console.log("close reserve");
+    res.style.display = 'none';
+    act.style.display = 'block';
+}
