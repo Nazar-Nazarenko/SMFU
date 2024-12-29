924471faed411e30d666cc2afb92b19ebf6c6d7b
nazarnazarenko <nanazarenko@deloitte.com>
2024-12-30 01:00:57 +0200

    extended fuctionality

--- static/main.js
+++ static/main.js
@@ -1,24 +1,64 @@
 const res = document.querySelector('.reserve-container')
-const act = document.querySelector('.action-container')
+const reserveMain = document.querySelector('.reserve-main');
+const reserveMainBlock = document.querySelector('.main-block');
+const menuSelect = document.querySelector('.menu-select');
+const homeScreen = document.querySelector('.home-screen');
+const act = document.querySelector('.action-container');
+const qrCode = document.querySelector('.qr-block');
+const toAllDetailsBtn = document.querySelector('.to-all-details-btn');
 
 function openReserve() {
-    console.log("open reserve");
-    console.log("close action");
+    reserveMain.style.display = 'block';
     act.style.display = 'none';
-    res.style.display = 'block';
+    homeScreen.style.display = 'none';
 }
 
 function openAction() {
-    console.log("open action");
-    console.log("close reserve");
-    res.style.display = 'none';
+    reserveMain.style.display = 'none';
     act.style.display = 'block';
 }
 
 function closeReserve() {
-    res.style.display = 'none';
+    reserveMain.style.display = 'none';
+    homeScreen.style.display = 'block';
+    if (res.style.display === 'block') {
+        res.style.display = 'none';
+        reserveMainBlock.style.display = 'block';
+        reserveMain.style.display = 'block';
+        homeScreen.style.display = 'none';
+        menuSelect.style.display = 'none';
+    }
 }
 
 function closeAction() {
     act.style.display = 'none';
+}
+
+function closeReserveMain() {
+    reserveMain.style.display = 'none';
+    homeScreen.style.display = 'block';
+}
+
+function openAllReserveInfo() {
+    reserveMainBlock.style.display = 'none';
+    res.style.display = 'block';
+}
+function openAllReserveInfoMenu() {
+    menuSelect.style.display = 'block';
+    toAllDetailsBtn.style.display = 'block';
+}
+
+function openQrCode() {
+    qrCode.style.display = 'block';
+    reserveMainBlock.style.display = 'none';
+    if (menuSelect.style.display === 'block') {
+        menuSelect.style.display = 'none';
+        qrCode.style.display = 'none';
+        reserveMainBlock.style.display = 'block';
+    }
+}
+
+function closeQrCode() {
+    qrCode.style.display = 'none';
+    reserveMainBlock.style.display = 'block';
 }
